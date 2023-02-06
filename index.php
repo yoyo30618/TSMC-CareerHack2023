d92860cbe50dbe598fb7fd431b59f14acbcf0b6c
yoyo30618 <yoyo30618@gmail.com>
2023-02-07 04:35:20 +0800

    初步完成初版首頁 修正許多css

---
+++ index.php
@@ -0,0 +1,400 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <title>TSMC-停車場管理系統</title>
+  <meta name="description" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+  
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/fontawesome.min.css" />
+  <link rel="stylesheet" href="css/flaticon.css" />
+  <link rel="stylesheet" href="css/selectize.css" />
+  <link rel="stylesheet" href="css/owl.carousel.min.css" />
+  <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+  <link rel="stylesheet" href="css/animate.min.css" />
+  <link rel="stylesheet" href="css/venobox.css" />
+  <link rel="stylesheet" href="style.css" />
+  <script src="js/vendor/modernizr-custom.js"></script>
+  </head>
+ <body>
+  <div class="wrapper-content">
+   <div class="wrapper">
+    <header class="header">
+     <div class="header-top-area">
+      <div class="container">
+       <div class="row">
+        <div class="col-md-12">
+         <div class="header-top">
+          <div class="tag-text">
+           <span>台灣積體電路製造股份有限公司 TSMC</span>
+          </div>
+          <div class="certified-text">
+           <span> (臺灣證券交易所代碼：2330，美國NYSE代碼：TSM)</span>
+          </div>
+          <div class="social-area">
+           <ul class="social-list list-style">
+            <li>
+              <a href="https://www.facebook.com/TSMCRecruiterExpress/" target="_blank">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/TWSemicon" target="_blank">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+           </ul>
+          </div>
+         </div>
+        </div>
+       </div>
+      </div>
+     </div>
+     <div class="header-bottom">
+      <div class="container">
+       <div class="row">
+        <div class="header-info">
+         <nav class="navbar navbar-expand-lg navbar-dark">
+          <a href="index.html"><img src="img/logo/logo.png" alt="logo" /></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+           <ul class="navbar-nav">
+            <li class="nav-item active"><a class="nav-link" href="index.html">首頁</a></li>
+            <li class="nav-item"><a class="nav-link" href="about.html">停車場狀態</a></li>
+            <li class="nav-item"><a class="nav-link" href="about.html">尋找愛車</a></li>
+            <li class="nav-item"><a class="nav-link" href="contact.html">預約VIP車位</a></li>
+            <li class="nav-item"><a class="nav-link" href="contact.html">後臺管理</a></li>
+            <li class="nav-item"><a class="nav-link" href="contact.html">黑白名單設定</a></li>
+            <li class="nav-item">
+             <div class="login-btn">
+              <a class="btn" href="#">登入</a>
+             </div>
+            </li>
+           </ul>
+          </div>
+         </nav>
+        </div>
+       </div>
+      </div>
+     </div>
+    </header>
+    <!-- 開頭橫幅開始 -->
+    <section class="intro-area">
+     <div class="container">
+      <div class="row">
+       <div class="col-md-12 col-lg-5 col-xl-6">
+        <div class="intro-content">
+         <h1 class="intro-title"></h1>
+         <p>此為2023 TSMC IT CareerHack中為了台積電所設計的專屬智慧停車管理系統。
+          <br>環境模擬於台積電的四處停車場，搭配影像辨識車牌，盼能妥善的運用車位庫存，使開車員工到達辦公場所快速、有效率地的滿足其停車需求,並同時節省警勤巡檢人力。</p>
+         <a class="btn" href="#">停車場狀態</a>
+        </div>
+       </div>
+       <div class="col-md-12 col-lg-7 col-xl-6">
+        <div class="intro-image-block">
+         <div class="shape-image-1 wow fadeInDown" data-wow-delay="0.5s">
+          <img src="img/hero/vector-1.png" alt="shape" />
+         </div>
+         </div>
+        </div>
+       </div>
+      </div>
+     </div>
+    </section>
+    <!-- 開頭橫幅結束 -->
+    <!-- 系統特點開始 -->
+    <section class="services-area section-padding">
+     <div class="container">
+      <div class="row">
+       <div class="col-md-6 col-lg-4">
+        <div class="service-single">
+         <div class="service-icon">
+          <img src="img/services/1.png" alt="Service" />
+         </div>
+         <h3>便利協尋</h3>
+         <p>鍵入車牌後可迅速找尋愛車所在位置<br>讓你不在為了找車而煩惱</p>
+        </div>
+       </div>
+       <div class="col-md-6 col-lg-4">
+        <div class="service-single">
+         <div class="service-icon">
+          <img src="img/services/2.png" alt="Service" />
+         </div>
+         <h3>方便控管</h3>
+         <p>管理方可以清楚了解各車位使用情況<br>也可妥善運用停車場空間</p>
+        </div>
+       </div>
+       <div class="col-md-6 col-lg-4">
+        <div class="service-single">
+         <div class="service-icon">
+          <img src="img/services/3.png" alt="Service" />
+         </div>
+         <h3>VIP車位服務</h3>
+         <p>到訪本公司的貴客<br>可在此系統為您的愛車預約一個專屬車位</p>
+        </div>
+       </div>
+      </div>
+     </div>
+    </section>
+    <!-- 系統特點結束 -->
+    <!-- 停車場位址開始 -->
+    <section class="project-area">
+     <div class="container-fluid">
+      <div class="row">
+       <div class="col-md-12">
+        <div class="section-title">
+         <h2>您的愛車可至以下四座停車場停放</h2>
+        </div>
+       </div>
+      </div>
+      <div class="row grid">
+       <div class="col-md-6 col-lg-3">
+        <div class="grid-item mouse-move">
+         <a href="#"><img src="img/projects/1.jpg" alt="Project" /></a>
+         <div class="grid-hover">
+          <h3>A停車場</h3>
+          <span>台積電F12P1-張忠謀大樓</span>
+         </div>
+         <div class="grid-hover-link">
+          <a href="#"><span class="flaticon-unlink"></span></a>
+          <a class="venobox vbox-item" data-gall="myGallery" data-title="A停車場" href="img/projects/1.jpg"><span class="flaticon-thin-expand-arrows"></span></a>
+         </div>
+        </div>
+       </div>
+       <div class="col-md-6 col-lg-3">
+        <div class="grid-item mouse-move">
+         <a href="#"><img src="img/projects/2.jpg" alt="Project" /></a>
+         <div class="grid-hover">
+          <h3>B停車場</h3>
+          <span>台積電五廠</span>
+         </div>
+         <div class="grid-hover-link">
+          <a href="#"><span class="flaticon-unlink"></span></a>
+          <a class="venobox vbox-item" data-gall="myGallery" data-title="B停車場" href="img/projects/2.jpg"><span class="flaticon-thin-expand-arrows"></span></a>
+         </div>
+        </div>
+       </div>
+       <div class="col-md-6 col-lg-3">
+        <div class="grid-item mouse-move">
+         <a href="#"><img src="img/projects/3.jpg" alt="Project" /></a>
+         <div class="grid-hover">
+          <h3>C停車場</h3>
+          <span>台積電十二廠P6</span>
+         </div>
+         <div class="grid-hover-link">
+          <a href="#"><span class="flaticon-unlink"></span></a>
+          <a class="venobox vbox-item" data-gall="myGallery" data-title="C停車場" href="img/projects/3.jpg"><span class="flaticon-thin-expand-arrows"></span></a>
+         </div>
+        </div>
+       </div>
+       <div class="col-md-6 col-lg-3">
+        <div class="grid-item mouse-move">
+         <a href="#"><img src="img/projects/4.jpg" alt="Project" /></a>
+         <div class="grid-hover">
+          <h3>D停車場</h3>
+          <span>台積電十二廠P4,5</span>
+         </div>
+         <div class="grid-hover-link">
+          <a href="#"><span class="flaticon-unlink"></span></a>
+          <a class="venobox vbox-item" data-gall="myGallery" data-title="D停車場" href="img/projects/4.jpg"><span class="flaticon-thin-expand-arrows"></span></a>
+         </div>
+        </div>
+       </div>
+
+      </div>
+     </div>
+    </section>
+    <!-- 停車場位址結束 -->
+    <!-- 台積電簡介開始 -->
+    <section class="analysis-area section-padding">
+     <div class="container">
+      <div class="row">
+       <div class="col-md-12 col-lg-12 col-xl-6">
+        <div class="analysis-mask-block">
+         <img src="img/services/shape.png" alt="img" />
+         <div class="analysis-mask">
+          <div class="analysis-mask-image">
+           <img src="img/services/4.jpg" alt="img" />
+          </div>
+         </div>
+        </div>
+       </div>
+       <div class="col-md-12 col-lg-12 col-xl-6">
+        <div class="analysis-content">
+         <h2>台積電集團</h2>
+         <p>
+           　　台灣積體電路製造(Taiwan Semiconductor Manufacturing Co., Ltd.)，簡稱台積電或TSMC，與旗下公司合稱時則稱作台積電集團<br>
+           　　是臺灣一家從事晶圓代工的公司，為全球規模最大的半導體製造廠，可量產的邏輯IC為全球最先進的製程，總部位於新竹科學園區，主要廠房則分布於臺灣的新竹、臺中、臺南等科學園區<br>
+           　　2021年8月，台積電在美國《財富》雜誌評選「全球最大500家公司」排行榜中，依營收規模名列全球第251名。2019年8月，台積電在PwC發表的「全球頂尖100家公司」排行榜中，依公司市場價值名列全球第37名
+         </p>
+         <div class="about-count">
+          <div class="counter-content-single">
+           <h2><span class="timer" data-from="1" data-to="1314" data-speed="5000" data-refresh-interval="50"></span></h2>
+           <h4>停車場可用空位</h4>
+          </div>
+          <div class="counter-content-single">
+           <h2><span class="timer" data-from="1" data-to="520" data-speed="5000" data-refresh-interval="50"></span></h2>
+           <h4>VIP車位可預約空位</h4>
+          </div>
+          <div class="counter-content-single">
+           <h2><span class="timer" data-from="1" data-to="99" data-speed="5000" data-refresh-interval="50"></span></h2>
+           <h4>已完成停車紀錄</h4>
+          </div>
+         </div>
+        </div>
+       </div>
+      </div>
+     </div>
+    </section>
+    <!-- 台積電簡介結束 -->
+    <!-- 開發團隊開始 -->
+    <section class="service-provide-area">
+     <div class="container">
+      <div class="row">
+       <div class="col-md-12">
+        <div class="section-title">
+         <h2>系統開發團隊</h2>
+         <div class="section-line">
+          <span></span>
+         </div>
+         <p>本系統由四位來自 國立臺灣海洋大學 與 國立臺東大學 的同學開發而成</p>
+        </div>
+       </div>
+      </div>
+      <div class="row">
+       <div class="col-md-12">
+        <div class="service-provide-block">
+         <div class="service-provide-single">
+          <img src="img/hero/1.png" alt="img" />
+          <h4><a href="#">劉紀佑</a></h4>
+          <p>國立臺東大學<br>資訊工程學系<br>碩士二年級</p>
+         </div> 
+         <div class="service-provide-single">
+          <img src="img/hero/2.png" alt="img" />
+          <h4><a href="#">吳振榮</a></h4>
+          <p>國立臺灣海洋大學<br>資訊工程學系<br>碩士一年級</p>
+         </div>
+         <div class="service-provide-single">
+          <img src="img/hero/3.png" alt="img" />
+          <h4><a href="#">葉航葦</a></h4>
+          <p>國立臺灣海洋大學<br>資訊工程學系<br>碩士一年級</p>
+         </div>
+         <div class="service-provide-single">
+          <img src="img/hero/4.png" alt="img" />
+          <h4><a href="#">曾宥慈</a></h4>
+          <p>國立臺東大學<br>資訊工程學系<br>大學四年級</p>
+         </div>
+        </div>
+       </div>
+      </div>
+     </div>
+    </section>
+    <!-- 開發團隊結束 -->
+    <!-- 停車場連結開始 -->
+    <div class="client-log-area section-padding">
+     <div class="container">
+      <div class="row">
+       <div class="col-md-12">
+        <div class="client-logo-slider owl-carousel owl-theme">
+         <div class="client-logo-single">
+          <a href="#"><img src="img/clients/1.jpg" alt="Logo" /></a>
+         </div>
+         <div class="client-logo-single">
+          <a href="#"><img src="img/clients/2.jpg" alt="Logo" /></a>
+         </div>
+         <div class="client-logo-single">
+          <a href="#"><img src="img/clients/3.jpg" alt="Logo" /></a>
+         </div>
+         <div class="client-logo-single">
+          <a href="#"><img src="img/clients/4.jpg" alt="Logo" /></a>
+         </div>
+        </div>
+       </div>
+      </div>
+     </div>
+    </div>
+    <!-- 停車場連結結束 -->
+    <!-- 底部開始 -->
+    <footer class="footer-area">
+     <div class="footer-top">
+      <div class="container">
+       <div class="row">
+        <div class="col-md-6 col-lg-2">
+         <div class="footer-info">
+         </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+         <div class="footer-info">
+          <div class="footer-logo">
+           <a href="index.html"><img src="img/logo/logo.png" alt="Logo" /></a>
+          </div>
+          <p>此為2023 TSMC IT CareerHack中為了台積電所設計的專屬智慧停車管理系統。<br>
+             環境模擬於台積電的四處停車場，搭配影像辨識車牌，盼能妥善的運用車位庫存，使開車員工到達辦公場所快速、有效率地的滿足其停車需求，並同時節省警勤巡檢人力。</p>
+         </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+         <div class="footer-info">
+         </div>
+        </div>
+        <div class="col-md-6 col-lg-4">
+         <div class="footer-info">
+          <h4>快速連結</h4>
+          <ul class="link-list list-style">
+           <li><a href="#">停車場狀態</a></li>
+           <li><a href="#">尋找愛車</a></li>
+           <li><a href="#">預約VIP車位</a></li>
+           <li><a href="#">後臺管理</a></li>
+           <li><a href="#">黑白名單設定</a></li>
+          </ul>
+        </div>
+       </div>
+      </div>
+     </div>
+     <div class="footer-bottom">
+      <div class="container">
+       <div class="row">
+        <div class="col-md-12">
+         <div class="footer-bottom-content">
+          <p>Copyright &copy; 台灣積體電路製造股份有限公司 著作權所有</p>
+         </div>
+        </div>
+       </div>
+      </div>
+     </div>
+     <!-- Start Back To Top Area -->
+     <div class="back-to-top">
+      <span>Back To Top</span>
+     </div>
+     <!-- End Back To Top Area -->
+    </footer>
+    <!-- 底部結束 -->
+   </div>
+  </div>
+  <!-- 指標圓球開始 -->
+  <div class="custom-cursor">
+   <div id="cursor">
+    <div id="cursor-ball"></div>
+   </div>
+  </div>
+  <!-- 指標圓球結束 -->
+  <!-- JS -->
+  <script src="js/jquery-3.1.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/selectize.js"></script>
+  <script src="js/icheck.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.appear.js"></script>
+  <script src="js/jquery.easing.1.3.min.js"></script>
+  <script src="js/venobox.min.js"></script>
+  <script src="js/jquery.countTo.js"></script>
+  <script src="js/wow.min.js"></script>
+  <script src="js/loadMoreResults.js"></script>
+  <script src="js/TweenMax.js"></script>
+  <script src="js/main.js"></script> 
+ </body>
+</html>
